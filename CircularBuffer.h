8b392227c46aeab7849bc55bd59341d95c5f52a7
sherzaad <57634578+sherzaad@users.noreply.github.com>
2019-11-21 11:58:53 +0000

    Add files via upload

---
+++ CircularBuffer.h
@@ -0,0 +1,126 @@
+#ifndef	CIRCULARBuffer_H
+#define	CIRCULARBuffer_H
+
+#include <Arduino.h>
+
+#define BUF_TYPE uint8_t //data type of  RBuf_t variables
+#define ARR_TYPE uint8_t //data type for RBuf_t pointer array
+
+#define IsEmpty -1
+#define IsFull -2
+
+typedef struct BufS
+{
+  ARR_TYPE *arr;
+  BUF_TYPE head;
+  BUF_TYPE tail;
+  BUF_TYPE count;
+  BUF_TYPE Size;
+} RBuf_t;
+
+class CircularBuffer {
+  private:
+    RBuf_t Buffer;
+
+
+  public:
+    //return status of buffer
+    inline int8_t Status()
+    {
+      if (Buffer.count == Buffer.Size) return -2; //buffer overflow
+      else if (Buffer.count == 0) return -1; //buffer empty
+
+      return 1; //buffer contains unread data
+    }
+
+    //return element from Buffer
+    inline int8_t Get(ARR_TYPE &c)
+    {
+      if (Buffer.count > 0) {
+        c = Buffer.arr[Buffer.tail];
+
+        ++Buffer.tail;
+        if (Buffer.tail == Buffer.Size) Buffer.tail = 0;
+
+        --Buffer.count;
+
+        return 1;
+      }
+
+      return -1; //Buffer is empty
+    }
+
+    //add element to Buffer
+    inline int8_t Put(ARR_TYPE c)
+    {
+      if (Buffer.count < Buffer.Size) {
+        Buffer.arr[Buffer.head] = c;
+
+        ++Buffer.head;
+        if (Buffer.head == Buffer.Size) Buffer.head = 0;
+
+        ++Buffer.count;
+
+        return 1;
+      }
+
+      return -2; //Buffer is full
+    }
+
+    //clear Buffer
+    inline void Flush (uint8_t clearBuffer = 0)
+    {
+      Buffer.count  = 0;
+      Buffer.head   = 0;
+      Buffer.tail   = 0;
+
+      if (clearBuffer)
+      {
+        memset (Buffer.arr, 0, sizeof(Buffer.arr));
+      }
+    }
+
+    //release memory
+    inline void FreeBuffer()
+    {
+      free(Buffer.arr);
+    }
+
+    //return Size of Buffer initialised
+    inline BUF_TYPE BufferSize()
+    {
+      return Buffer.Size;
+    }
+
+    //returns buffer current usage
+    inline BUF_TYPE BufferCount()
+    {
+      return Buffer.count;
+    }
+
+    //constructor
+    CircularBuffer(BUF_TYPE Size)
+    {
+      Buffer.Size = Size;
+
+      Buffer.arr = malloc(sizeof(ARR_TYPE) * Buffer.Size);
+
+      /*****
+        The following clears:
+      	-> arr
+      	-> head
+      	-> tail
+      	-> count
+        and sets head = tail
+      ***/
+      Flush();
+    }
+
+    //deconstructor
+    ~CircularBuffer()
+    {
+      free(Buffer.arr);
+    }
+};
+
+#endif
---
+++ RingBuffer.ino
@@ -0,0 +1,63 @@
+#include "CircularBuffer.h"
+
+CircularBuffer Fifo(25);
+
+void setup() {
+  Serial.begin(115200);
+
+  delay(1000);
+
+  randomSeed(analogRead(0)); //allows a random start
+
+  Serial.print("Fifo Size: ");
+  Serial.println(Fifo.BufferSize());
+
+  Serial.print("Fifo Usage: ");
+  Serial.print(Fifo.BufferCount());
+  Serial.print(" of ");
+  Serial.println(Fifo.BufferSize());
+  Serial.print("Fifo Status: ");
+  Serial.println(Fifo.Status());
+
+  Serial.println("Filling Fifo....");
+
+  uint8_t randNumber;
+
+  for (uint8_t i = 0; i < Fifo.BufferSize() + 4; ++i) { //+4 to demonstrate overflow
+    //generate random number between 0 and Fifo.BufferSize()-1 (inclusive)
+    randNumber = random(0, Fifo.BufferSize());
+    Serial.print(randNumber);
+    Serial.print(" ");
+    Fifo.Put(randNumber);
+  }
+
+  Serial.print("\nFifo Usage: ");
+  Serial.print(Fifo.BufferCount());
+  Serial.print(" of ");
+  Serial.println(Fifo.BufferSize());
+  Serial.print("Fifo Status: ");
+  Serial.println(Fifo.Status());
+
+  Serial.println("Reading from Fifo....");
+
+  uint8_t data;
+
+  for (uint8_t i = 0; i < Fifo.BufferSize() + 4; ++i) { //+4 to demonstrate no rollover if overflow has occurred
+    Fifo.Get(data);
+    Serial.print(data);
+    Serial.print(" ");
+  }
+
+  Serial.print("\nFifo Usage: ");
+  Serial.print(Fifo.BufferCount());
+  Serial.print(" of ");
+  Serial.println(Fifo.BufferSize());
+  Serial.print("Fifo Status: ");
+  Serial.println(Fifo.Status());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
